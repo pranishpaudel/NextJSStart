d698afc22ce918300e9bcd7f5c69712bf382cff6
Insahinda5656 <insahinda@gmail.com>
2024-03-09 13:42:48 +0545

    fix: :bug: Fix bug

--- app/(addnewuser)/page.jsx
+++ app/(addnewuser)/page.jsx
@@ -1,50 +1,50 @@
-"use client";
+// "use client";
 
-import { useState } from "react";
-import { useEffect } from "react";
+// import { useState } from "react";
+// import { useEffect } from "react";
 
-function addNewUser() {
-  const [name, setName] = useState(null);
-  const [age, setAge] = useState(null);
-  const [email, setEmail] = useState(null);
+// function addNewUser() {
+//   const [name, setName] = useState(null);
+//   const [age, setAge] = useState(null);
+//   const [email, setEmail] = useState(null);
 
-  //   const [count, setCount] = useAtom(countAtom);
-  //   const [userProfile, setUserProfile] = useAtom(userProfileAtom);
-  const addNewUserHandler = async () => {
-    // setCount(count + 5);
-    // console.log(name, age, email, userProfile);
-    let response = await fetch("api/users", {
-      method: "POST",
-      body: JSON.stringify({ name, age, email }),
-    });
-    response = await response.json();
-    console.log(response);
-    if (response.res === "User validation was successful") {
-      alert("User added successfully");
-    } else [alert("User not added")];
-  };
-  //   useEffect(() => {
-  //     console.log(`Current count is: ${count}`);
-  //   }, [count]);
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter your name"
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="Enter your age"
-        onChange={(e) => setAge(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Enter your email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <button onClick={addNewUserHandler}> AddBtn </button>
-    </div>
-  );
-}
-export default addNewUser;
+//   //   const [count, setCount] = useAtom(countAtom);
+//   //   const [userProfile, setUserProfile] = useAtom(userProfileAtom);
+//   const addNewUserHandler = async () => {
+//     // setCount(count + 5);
+//     // console.log(name, age, email, userProfile);
+//     let response = await fetch("api/users", {
+//       method: "POST",
+//       body: JSON.stringify({ name, age, email }),
+//     });
+//     response = await response.json();
+//     console.log(response);
+//     if (response.res === "User validation was successful") {
+//       alert("User added successfully");
+//     } else [alert("User not added")];
+//   };
+//   //   useEffect(() => {
+//   //     console.log(`Current count is: ${count}`);
+//   //   }, [count]);
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         placeholder="Enter your name"
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <input
+//         type="number"
+//         placeholder="Enter your age"
+//         onChange={(e) => setAge(e.target.value)}
+//       />
+//       <input
+//         type="text"
+//         placeholder="Enter your email"
+//         onChange={(e) => setEmail(e.target.value)}
+//       />
+//       <button onClick={addNewUserHandler}> AddBtn </button>
+//     </div>
+//   );
+// }
+// export default addNewUser;
--- app/page.js
+++ app/page.js
@@ -1,20 +1,25 @@
-'use client'
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-import { useRouter } from "next/navigation"
+function Page() {
+  const router = useRouter();
 
-function page() {
+  useEffect(() => {
+    // Your side effects code here, if needed
+  }, []); // Dependency array
 
-  
-  const router= useRouter()
   return (
-  <section>
-    <h1>Google Fonts</h1>
-    <h2>Hello this is a roboto font</h2>
-    <button className="text-blue-600 h-10 w-20 shadow-lg rounded-md hover:font-extrabold" onClick={(e)=>router.push('about')}>Click me</button>
-  </section>
-  )
+    <section>
+      <h1>Google Fonts</h1>
+      <h2>Hello, this is a Roboto font</h2>
+      <button
+        className="text-blue-600 h-10 w-20 shadow-lg rounded-md hover:font-extrabold"
+        onClick={(e) => router.push("about")}
+      >
+        Click me
+      </button>
+    </section>
+  );
 }
 
-export default page
-
-
+export default Page;
